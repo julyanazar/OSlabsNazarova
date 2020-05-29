32b4ab34c1d2524bf58c6fa2cc4c24a69de9fc71
julyanazar <nnwrnuwr@gmail,com>
2020-05-29 15:04:03 +0400

    второй шанс

--- OSlab3/src/SubstitutionAlgorythm.java
+++ OSlab3/src/SubstitutionAlgorythm.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class SubstitutionAlgorythm {
-
+    // алгоритм второй шанс
     ArrayList<Record> Table;
     PhysicalMemory physicalMemory;
 
@@ -9,27 +9,23 @@ public class SubstitutionAlgorythm {
         this.Table = Table;
         this.physicalMemory = physicalMemory;
     }
-
     public void start() {
-        for (int i = 0; i < Table.size(); i++) {
+        for(int i = 0; i< Table.size(); i++) {
             int number = Table.get(i).getNumberOfPhysicalPage();
-            if (Table.get(i).r() == false && Table.get(i).isInPhysicalMemory()) {
-                if (Table.get(i).isModified() == false) {
-                    System.out.println("Страница № " + Table.get(i).getPageNumber() +
-                            "удалена из памяти, так признак обращения сброшен и страница не была модифицирована");
-                    physicalMemory.removePage(number);
-                } else {
-                    System.out.println("Страница № " + Table.get(i).getPageNumber() + "выгружена на диск,"
-                            + " так как признак обращения сброшен");
-                    physicalMemory.sendPageToDisk(number);
-                }
+            if(Table.get(i).r() == false && Table.get(i).isInPhysicalMemory()) {
+                System.out.println("Страница № " + Table.get(i).getPageNumber() + "выгружена на диск," + " так как признак обращения сброшен");
+                physicalMemory.sendPageToDisk(number);
                 Table.get(i).removeFromPhysicalMemory();
                 Table.get(i).setNumberOfPhysicalPage(-1);
                 break;
-            } else {
-                Table.get(i).r(false);
-                System.out.println("Страница № " + Table.get(i).getPageNumber() + "признак обращения сброшен");
             }
+            else {
+                Table.get(i).r(false);
+                Table.add(Table.get(i));
+                Table.remove(i);
+                System.out.println("Страница № " + Table.get(i).getPageNumber() + "перемещена в конец очереди, признак обращения сброшен");
+            }
+
 
         }
     }
